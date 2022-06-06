75a0adc2e5660ff8cb86fad1617469394d5744b1
Anton Pershin <tony.pershin@gmail.com>
2022-06-06 19:08:16 +0300

    Change fit() and predict() interface to (X, y) representation Fixes #6

--- networkts/forecasters/prophet.py
+++ networkts/forecasters/prophet.py
@@ -1,41 +1,41 @@
+import logging
+
 from prophet import Prophet
 import pandas as pd
 
-from networkts.base import BaseForecaster
+from networkts.base import BaseForecaster, Timeseries, as_numpy_array, take_time_array
 from networkts.utils.convert_time import time
 
 
 class NtsProphet(BaseForecaster):
+    LOGGER = logging.getLogger(__qualname__)
+
     def __init__(self, yearly_seasonality=False, weekly_seasonality=False):
         self.yearly_seasonality = yearly_seasonality
         self.weekly_seasonality = weekly_seasonality
-
-        self._is_fitted = False
-
-        self._y = None
-        self._X = None
-
-        # forecasting horizon
-        self._fh = None
-        self._cutoff = None  # reference point for relative fh
-
         super(BaseForecaster, self).__init__()
 
-    def _fit(self, y, X):
+    def _fit(
+        self,
+        X: Timeseries,
+        y: Timeseries,
+    ):
         train = pd.DataFrame(columns=['ds', 'y'])
-        train['ds'] = [time(el) for el in X]
-        train['y'] = y
-        self.model = Prophet(
+        train['ds'] = [time(el) for el in take_time_array(X)]
+        train['y'] = as_numpy_array(y)
+        self._model = Prophet(
                         yearly_seasonality=self.yearly_seasonality,
                         weekly_seasonality=self.weekly_seasonality
                         ).fit(train)
         return self
 
-    def _predict(self, n_timesteps, X=None):
-        test = pd.DataFrame([time(el) for el in X], columns=['ds'])
-        y_pred = self.model.predict(test)['yhat'].values
+    def _predict(
+        self,
+        X: Timeseries,
+    ):
+        test = pd.DataFrame(
+            [time(el) for el in take_time_array(X)],
+            columns=['ds']
+        )
+        y_pred = self._model.predict(test)['yhat'].values
         return y_pred
-
-    def _update(self, y, X):
-        self.model = self.fit(self, y=y, X=X)
-        return self
--- networkts/forecasters/xgboost.py
+++ networkts/forecasters/xgboost.py
@@ -1,36 +1,38 @@
+import logging
+
 import xgboost as xgb
 import pandas as pd
 
-from networkts.base import BaseForecaster
+from networkts.base import BaseForecaster, Timeseries, as_numpy_array, take_time_array
 from networkts.utils.convert_time import time
 from networkts.utils.create_features import create_features
 
 
 class NtsXgboost(BaseForecaster):
+    LOGGER = logging.getLogger(__qualname__)
+
     def __init__(self):
-        self._is_fitted = False
-
-        self._y = None
-        self._X = None
-
-        # forecasting horizon
-        self._fh = None
-        self._cutoff = None  # reference point for relative fh
-
         super(BaseForecaster, self).__init__()
 
-    def _fit(self, y, X):
+    def _fit(
+        self,
+        X: Timeseries,
+        y: Timeseries,
+    ):
         # y - array with traffic
         # X - array with temporal features (count of mins)
-        train_x = create_features([time(el) for el in X])
-        self.model = xgb.XGBRegressor().fit(train_x, y, verbose=False)
+        train_x = create_features([time(el) for el in take_time_array(X)])
+        self.model = xgb.XGBRegressor().fit(
+            train_x,
+            as_numpy_array(y),
+            verbose=False
+        )
         return self
 
-    def _predict(self, n_timesteps, X=None):
-        test_x = create_features([time(el) for el in X])
+    def _predict(
+        self,
+        X: Timeseries,
+    ):
+        test_x = create_features([time(el) for el in take_time_array(X)])
         y_pred = self.model.predict(test_x)
         return y_pred
-
-    def _update(self, y, X):
-        self.model = self.fit(self, y=y, X=X)
-        return self
