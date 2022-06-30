383c0c5d5ef83330ffbb33d688312939b8dc8604
Knyazev Nikita WX1105301 <knyazev.nikita@huawei-partners.com>
2022-06-30 12:27:18 +0300

    Updated plot_score

--- networkts/plots/validation.py
+++ networkts/plots/validation.py
@@ -5,39 +5,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_valid(
-                file: str,
-                save_path: str = None,
-                ):
-    f = open(file, "r")
-    mae = []
-    mape = []
-    for row in f.readlines()[:-6]:
-        score = row[:-1].split(' ')
-        mape.append(float(score[0]))
-        mae.append(float(score[1]))
-    f.close()
+def plot_valid_score(
+        file_name: str,
+        title: str = None,
+        fig_name: str = None,
+        ):
+    with open(file_name, 'rb') as f:
+        score = pickle.load(f)
+    mape = score['Mape']
+    mae = score['Mae']
 
-    plt.figure(figsize=(18, 6))
-    plt.plot(mape)
-    plt.axhline(y=np.median(mape), color='r', linestyle='-')
-    plt.xlabel("iteration")
-    plt.ylabel("mape")
-    plt.ylim(0, 2)
-    plt.legend(['mape', 'median'])
-    plt.title("Validation MAPE")
-    if save_path is not None:
-        plt.savefig(os.path.join(save_path, 'mape.png'))
+    fig, ax = plt.subplots(2, 1, sharex=True, figsize=(22, 14))
+    if title is not None:
+        fig.suptitle(title)
+    ax[0].plot(mape, color='orange', label='mape')
+    ax[0].axhline(
+        y=np.median(mape),
+        lw=1,
+        color='r',
+        linestyle='-',
+        label='median'
+        )
+    ax[0].set_ylabel("mape")
+    ax[0].set_yscale('log')
+    ax[0].legend()
+    ax[0].set_title("Validation MAPE")
 
-    plt.figure(figsize=(18, 6))
-    plt.plot(mae)
-    plt.axhline(y=np.median(mae), color='r', linestyle='-')
-    plt.xlabel("iteration")
-    plt.ylabel("mae")
-    plt.legend(['mae', 'median'])
-    plt.title("Validation MAE")
-    if save_path is not None:
-        plt.savefig(os.path.join(save_path, 'mae.png'))
+    ax[1].plot(mae, color='orange', label='mae')
+    ax[1].axhline(
+        y=np.median(mae),
+        lw=1,
+        color='r',
+        linestyle='-',
+        label='median'
+        )
+    ax[1].set_ylabel("mae")
+    ax[1].set_xlabel("iteration")
+    ax[1].set_yscale('log')
+    ax[1].legend()
+    ax[1].set_title("Validation MAE")
+    if fig_name is not None:
+        plt.savefig(fig_name)
+    return fig, ax
 
 
 def plot_score_distribution_by_series(
