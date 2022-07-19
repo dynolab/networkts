039c7415293abbbb99b0642281f6611d3eb9fdbc
Knyazev Nikita WX1105301 <knyazev.nikita@huawei-partners.com>
2022-07-19 10:47:48 +0300

    Added LightGBM forecasring model. Closed #12

--- forecasting.py
+++ forecasting.py
@@ -7,119 +7,146 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 from sklearn.compose import TransformedTargetRegressor
+from threadpoolctl import ThreadpoolController, threadpool_limits
 
 from networkts.utils import common
 sys.path.append(common.CONF['directory']['path_networks'])
 from forecasters.autoreg import NtsAutoreg
 from forecasters.xgboost import NtsXgboost
 from forecasters.holtwinter import NtsHoltWinter
+from forecasters._lightgbm import NtsLightgbm
 from utils.sklearn_helpers import SklearnWrapperForForecaster, build_target_transformer
 from cross_validation import ValidationBasedOnRollingForecastingOrigin as Valid
-from decompositions.basic import log_target, inverse_log_target
+from decompositions.basic import *
 
 
-if __name__ == '__main__':
+nthread = common.CONF['multiprocessing']['nthread']
+controller = ThreadpoolController()
+@threadpool_limits.wrap(limits=nthread, user_api='blas')
+def func():
     warnings.filterwarnings('ignore')
-    
+
     df = pd.read_csv(
                 os.path.join(
                     os.getcwd(),
-                    common.CONF['datasets']['abilene']['root'],
-                    common.CONF['datasets']['abilene']['edges_traffic']
+                    common.CONF['datasets']['pemsd7']['root'],
+                    common.CONF['datasets']['pemsd7']['speeds_file']
                     ),
-                index_col=0,     # abilene, totem
-                # header=None,   # pemsd7  
+                # index_col=0,     # abilene, totem
+                header=None,   # pemsd7
                 )
 
-    df = df.replace([0], 0.1)
+    '''for feature in df.columns.values:
+        df.loc[df[feature] < 1000, feature] = 1000'''
 
-    train_size = 1000
     test_size = 500
     period = 288
     delta_time = 5
-
     ind = np.array([el*delta_time for el in range(df.shape[0])])
 
-    score_mape = []
-    score_mae = []
-    time = datetime.now()
-    for i, feature in enumerate(df.columns.values):
-        print(f'{i+1}/{len(df.columns.values)}')        
-        cross_val = Valid(
-                        n_test_timesteps=test_size,
-                        n_training_timesteps=train_size,
-                        n_splits=df.shape[0]//test_size - train_size//test_size,
-                        max_train_size=np.Inf
-                        )
-        # Holt-winter
-        '''
-        model = build_target_transformer(
+    # SSA
+    L = 50
+    n = 2
+
+    for train_size in [1000, 2000, 3000, 4000, 5000]:
+        score_mape = []
+        score_mae = []
+        time = datetime.now()
+        for i, feature in enumerate(df.columns.values):
+            print(f'{i+1}/{len(df.columns.values)}')
+            cross_val = Valid(
+                            n_test_timesteps=test_size,
+                            n_training_timesteps=train_size,
+                            n_splits=df.shape[0]//test_size - train_size//test_size,
+                            max_train_size=np.Inf
+                            )
+            # Holt-winter
+            '''
+            model = build_target_transformer(
+                            TransformedTargetRegressor,
+                            SklearnWrapperForForecaster(NtsHoltWinter(
+                                                        seasonal='additive',
+                                                        seasonal_periods=period
+                                                        )),
+                            func=log_target,
+                            inverse_func=inverse_log_target,
+                            params=None,
+                            inverse_params=None,
+                            )
+            '''
+
+            # XGB
+            '''
+            model = build_target_transformer(
+                                        TransformedTargetRegressor,
+                                        SklearnWrapperForForecaster(NtsXgboost(nthread=nthread)),
+                                        func=log_target,
+                                        inverse_func=inverse_log_target,
+                                        params=None,
+                                        inverse_params=None,
+                                        )
+            '''
+
+            # AR
+            '''
+            model = build_target_transformer(
                         TransformedTargetRegressor,
-                        SklearnWrapperForForecaster(NtsHoltWinter(
-                                                    seasonal='additive',
-                                                    seasonal_periods=period
-                                                    )),
+                        SklearnWrapperForForecaster(
+                            NtsAutoreg(
+                                lags=3,
+                                seasonal=True,
+                                period=period
+                                )),
                         func=log_target,
                         inverse_func=inverse_log_target,
                         params=None,
                         inverse_params=None,
                         )
-        '''
+            '''
 
-        # XGB
-        
-        model = build_target_transformer(
-                                    TransformedTargetRegressor,
-                                    SklearnWrapperForForecaster(NtsXgboost()),
-                                    func=log_target,
-                                    inverse_func=inverse_log_target,
-                                    params=None,
-                                    inverse_params=None,
-                                    )
-        
-
-        # AR
-        '''
-        model = build_target_transformer(
-                    TransformedTargetRegressor,
-                    SklearnWrapperForForecaster(
-                        NtsAutoreg(
-                            lags=3,
-                            seasonal=True,
-                            period=period
-                            )),
-                    func=log_target,
-                    inverse_func=inverse_log_target,
-                    params=None,
-                    inverse_params=None,
-                    )
-        '''
-        
-        t = cross_val.evaluate(
-                        forecaster=model,
-                        y=df[feature].values,
-                        X=ind
+            # Lgb
+            model = build_target_transformer(
+                        TransformedTargetRegressor,
+                        SklearnWrapperForForecaster(
+                            NtsLightgbm(
+                                num_round=1000,
+                                num_leaves=32,
+                                num_threads=nthread,
+                                learning_rate=0.1,
+                                )),
+                        func=log_target,
+                        inverse_func=inverse_log_target,
+                        params=None,
+                        inverse_params=None,
                         )
 
-        t = np.array(t)
-        t1, t2 = t[:, 0], t[:, 1]
-        score_mape += t1.tolist()
-        score_mae += t2.tolist()
-    
-    time = datetime.now() - time
+            t = cross_val.evaluate(
+                            forecaster=model,
+                            y=df[feature].values,
+                            X=ind
+                            )
 
-    score_mae = np.array(score_mae).reshape(-1)
-    score_mape = np.array(score_mape).reshape(-1)
+            t = np.array(t)
+            t1, t2 = t[:, 0], t[:, 1]
+            score_mape += t1.tolist()
+            score_mae += t2.tolist()
+        
+        time = datetime.now() - time
 
-    score_file_name = 'your file name'
-    score_dict = {
-        'Avg_mape': np.mean(score_mape),
-        'Mape_median': np.median(score_mape),
-        'Avg_mae': np.mean(score_mae),
-        'Mae_median': np.median(score_mae),
-        'Time': time.total_seconds(),
-        'Mape': score_mape,
-        'Mae': score_mae
+        score_mae = np.array(score_mae).reshape(-1)
+        score_mape = np.array(score_mape).reshape(-1)
+
+        score_dict = {
+            'Avg_mape': np.mean(score_mape),
+            'Mape_median': np.median(score_mape),
+            'Avg_mae': np.mean(score_mae),
+            'Mae_median': np.median(score_mae),
+            'Time': time.total_seconds(),
+            'Mape': score_mape,
+            'Mae': score_mae
         }
-    with open(score_file_name, 'wb') as file_pi:
-        pickle.dump(score_dict, file_pi)
+        with open(f'valid_results/PeMSD7/window/log/score_lgb_{train_size}', 'wb') as file_pi:
+            pickle.dump(score_dict, file_pi)
+    
+func()
+print('done')
---
+++ networkts/forecasters/_lightgbm.py
@@ -0,0 +1,56 @@
+import logging
+
+import lightgbm as lgb
+
+from networkts.base import BaseForecaster, Timeseries
+from networkts.base import as_numpy_array
+from networkts.utils.convert_time import time
+from networkts.utils.create_features import create_features
+
+
+class NtsLightgbm(BaseForecaster):
+    LOGGER = logging.getLogger(__qualname__)
+
+    def __init__(
+        self,
+        num_round=1000,
+        num_leaves=32,
+        num_threads=16,
+        learning_rate=0.1
+    ):
+        self._is_fitted = False
+        self.num_round = num_round
+        self.learning_rate = learning_rate
+        self.num_leaves = num_leaves
+        self.num_threads = num_threads
+        self._y = None
+        self._X = None
+        self.params = {
+            "objective": "regression",
+            "metric": ["mape", "mae"],
+            "num_leaves": self.num_leaves,
+            "num_threads": self.num_threads,
+            "learning_rate": self.learning_rate,
+            'verbose': -1,
+            }
+        super().__init__()
+
+    def _fit(
+        self,
+        X: Timeseries,
+        y: Timeseries,
+    ):
+        # y - array with traffic
+        # X - array with temporal features (count of mins)
+        train_x = create_features([time(el) for el in X])
+        train_data = lgb.Dataset(train_x, label=as_numpy_array(y))
+        self.model = lgb.train(self.params, train_data, self.num_round)
+        return self
+
+    def _predict(
+        self,
+        X: Timeseries,
+    ):
+        test_x = create_features([time(el) for el in X])
+        y_pred = self.model.predict(test_x)
+        return y_pred
