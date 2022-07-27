6342579a8db8d3a2e714bec16b3994a70b0ce8ec
Knyazev Nikita WX1105301 <knyazev.nikita@huawei-partners.com>
2022-07-27 18:04:31 +0300

    Adapted ES, Boxcox decompositions for multivariate case #20

--- networkts/decompositions/standard.py
+++ networkts/decompositions/standard.py
@@ -22,12 +22,21 @@ class BoxcoxTarget(object):
         super().__init__()
 
     def transform(self, y):
-        y = np.array(y).reshape(-1).tolist()
         self.is_fitted = True
-        if (len(y) < 1 or len(y) == y.count(y[0])):
-            res, self.lmbd = np.log(y), 0
-            return res
-        res, self.lmbd = stats.boxcox(y)
+        if len(y.shape) == 1:
+            y = y.tolist()
+            if (len(y) < 1 or len(y) == y.count(y[0])):
+                res, self.lmbd = np.log(y), 0
+                return res
+            res, self.lmbd = stats.boxcox(y)
+        else:
+            res = np.empty(y.shape)
+            self.lmbd = np.empty(y.shape[1])
+            for j in range(y.shape[1]):
+                if (len(y[:, j]) < 2 or len(y[:, j]) == list(y[:, j]).count(y[0, j])):
+                    res[:, j], self.lmbd[j] = np.log(y[:, j]), 0
+                    return res
+                res[:, j], self.lmbd[j] = stats.boxcox(y[:, j])
         return res
 
     def inverse_transform(self, y):
@@ -36,9 +45,20 @@ class BoxcoxTarget(object):
                 f"This instance of {self.__class__.__name__} has not "
                 f"been fitted yet; please call `target` first."
             )
-        if self.lmbd == 0:
-            return np.exp(y)
-        return np.power(abs(y*self.lmbd+1), 1/self.lmbd)
+
+        if isinstance(self.lmbd, int):
+            if self.lmbd == 0:
+                return np.exp(y)
+            res = np.power(abs(y*self.lmbd+1), 1/self.lmbd)
+
+        else:
+            res = np.empty(y.shape)
+            for j in range(y.shape[1]):
+                if self.lmbd[j] == 0:
+                    res[:, j] = np.exp(y[:, j])
+                else:
+                    res[:, j] = np.power(abs(y[:, j]*self.lmbd[j]+1), 1/self.lmbd[j])
+        return res
 
 
 class ESTarget(object):
@@ -50,11 +70,21 @@ class ESTarget(object):
         super().__init__()
 
     def transform(self, y):
-        s = ExponentialSmoothing(y).fit(
-                                    smoothing_level=self.smoothing_level,
-                                    optimized=False
-                                    )
-        return s.fittedvalues
+        if len(y.shape) < 2:
+            s = ExponentialSmoothing(y).fit(
+                                        smoothing_level=self.smoothing_level,
+                                        optimized=False
+                                        )
+            res = s.fittedvalues
+        else:
+            res = np.empty(y.shape)
+            for j in range(y.shape[1]):
+                s = ExponentialSmoothing(y[:, j]).fit(
+                                        smoothing_level=self.smoothing_level,
+                                        optimized=False
+                                        )
+                res[:, j] = s.fittedvalues
+        return res
 
     def inverse_transform(self, y):
         # do nothing
