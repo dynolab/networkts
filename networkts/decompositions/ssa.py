fe6fbfb5a55c11b3df45c67ddfa827a4372ff49f
Knyazev Nikita WX1105301 <knyazev.nikita@huawei-partners.com>
2022-07-27 15:41:38 +0300

    Updated SSA decomposition. Closed #19.

--- networkts/decompositions/ssa.py
+++ networkts/decompositions/ssa.py
@@ -3,68 +3,71 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+"""
+Decomposes the given time series with a singular-spectrum analysis.
+Assumes the values of the time series are recorded at equal intervals.
+
+Parameters
+----------
+tseries :   The original time series, in the form of a Pandas Series,
+            NumPy array or list.
+L : The window length. Must be an integer 2 <= L <= N/2, where N is the
+    length of the time series.
+save_mem :  Conserve memory by not retaining the elementary matrices.
+            Recommended for long time series with thousands of values.
+            Defaults to True.
+
+Note: Even if an NumPy array or list is used for the initial time
+series, all time series returned will be in the form of a Pandas
+Series or DataFrame object.
+"""
 class SSA(object):
     __supported_types = (pd.Series, np.ndarray, list)
 
     def __init__(
         self,
-        tseries,
         L: int = 50,
         noise_signal_split: int = 2,
         save_mem: bool = True
     ):
-        """
-        Decomposes the given time series with a singular-spectrum analysis.
-        Assumes the values of the time series are recorded at equal intervals.
+        self._L = L
+        self.noise_signal_split = noise_signal_split
+        self.save_mem = save_mem
+        self._N = None
+        self.orig_TS = None
+        self._K = None
+        self._V = None
+        self._X_elem = None
+        self.Wcorr = None
+        super().__init__()
 
-        Parameters
-        ----------
-        tseries :   The original time series, in the form of a Pandas Series,
-                    NumPy array or list.
-        L : The window length. Must be an integer 2 <= L <= N/2, where N is the
-            length of the time series.
-        save_mem :  Conserve memory by not retaining the elementary matrices.
-                    Recommended for long time series with thousands of values.
-                    Defaults to True.
-
-        Note: Even if an NumPy array or list is used for the initial time
-        series, all time series returned will be in the form of a Pandas
-        Series or DataFrame object.
-        """
-
+    def decompose_serie(self, tseries):
         # Tedious type-checking for the initial time series
         if not isinstance(tseries, self.__supported_types):
-            er = 'Unsupported time series object. '
-            er2 = 'Try Pandas Series, NumPy array or list.'
-            raise TypeError(er+er2)
+            raise TypeError(
+                "Unsupported time series object. "
+                "Try Pandas Series, NumPy array or list."
+                )
 
         # Checks to save us from ourselves
         self._N = len(tseries)
-        if not 2 <= L <= self._N/2:
-            er = "The window length must be in the interval [2, N/2]."
-            raise ValueError(er)
-
-        self._L = L
-        self.noise_signal_split = noise_signal_split
+        if not 2 <= self._L <= self._N/2:
+            raise ValueError(
+                "The window length must be in the interval [2, N/2]."
+            )
+        
         tseries = np.array(tseries).reshape(-1)
         self.orig_TS = pd.Series(tseries)
         self._K = self._N - self._L + 1
 
         # Embed the time series in a trajectory matrix
-        self._X = np.array([self.orig_TS.values[i:L+i]
+        self._X = np.array([self.orig_TS.values[i:self._L+i]
                             for i in range(0, self._K)]).T
 
         # Decompose the trajectory matrix
         self._U, self.Sigma, self._VT = np.linalg.svd(self._X)
         self._d = np.linalg.matrix_rank(self._X)
-
         self.TS_comps = np.zeros((self._N, self._d))
-
-        self.save_mem = save_mem
-        self._V = None
-        self._X_elem = None
-
-        self.Wcorr = None
 
     def save_memory(self):
         if not self.save_mem:
@@ -100,20 +103,23 @@ class SSA(object):
             # so we won't keep it.
             self._V = "Re-run with save_mem=False to retain the V matrix."
 
-    def components_to_df(self, n=0):
+    def components_to_df(self, noise_signal_split):
         """
         Returns all the time series components in a single
         Pandas DataFrame object.
         """
-        if n > 0:
-            n = min(n, self._d)
+        if noise_signal_split > 0:
+            noise_signal_split = min(noise_signal_split, self._d)
         else:
-            n = self._d
+            noise_signal_split = self._d
 
         # Create list of columns - call them F0, F1, F2, ...
-        cols = ["F{}".format(i) for i in range(n)]
-        return pd.DataFrame(self.TS_comps[:, :n], columns=cols,
-                            index=self.orig_TS.index)
+        cols = ["F{}".format(i) for i in range(noise_signal_split)]
+        return pd.DataFrame(
+                    self.TS_comps[:, :noise_signal_split],
+                    columns=cols,
+                    index=self.orig_TS.index
+                    )
 
     def reconstruct(self, indices):
         """
@@ -136,7 +142,6 @@ class SSA(object):
         """
         Calculates the w-correlation matrix for the time series.
         """
-
         # Calculate the weights
         el1 = list(np.arange(self._L)+1)
         el2 = [self._L]*(self._K-self._L-1)
@@ -190,13 +195,14 @@ class SSA(object):
         plt.xlim(min-0.5, max_rnge+0.5)
         plt.ylim(max_rnge+0.5, min-0.5)
 
-    def ssa_target(self, y):
+    def transform(self, y):
         y = np.array(y).reshape(-1)
+        self.decompose_serie(y)
         self.save_memory()
         self.calc_wcorr()
         res = np.array(abs(self.reconstruct(slice(0, self.noise_signal_split))))
         return res
     
-    def inverse_ssa_target(self, y):
+    def inverse_transform(self, y):
         # do nothing
         return y
