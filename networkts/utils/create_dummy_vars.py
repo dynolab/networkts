4f84ac0157c39f0c28a2ce91bd41bc0df9ccd1d1
Knyazev Nikita WX1105301 <knyazev.nikita@huawei-partners.com>
2022-08-16 17:34:16 +0300

    Added dummy variable generator for multivariate xgboost #13

--- networkts/utils/create_dummy_vars.py
+++ networkts/utils/create_dummy_vars.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+from networkts.utils.create_features import create_features
+from networkts.utils.convert_time import time
+
 
 def create_dummy_vars(
     index,
@@ -15,4 +18,17 @@ def create_dummy_vars(
         dummy_vars[f'dv_{(el)%period}'].iloc[el] = 1
     
     dummy_vars.set_index(index, inplace=True)
+    return dummy_vars
+
+def create_xgb_dummy_vars(
+    neighbors: pd.DataFrame,
+    window_size: int,
+):
+    delta_time = neighbors.index.values[1]-neighbors.index.values[0]
+    inds = [time(_*delta_time) for _ in range(neighbors.shape[0]+window_size)]
+    dummy_vars = create_features(inds).iloc[window_size:, :]
+    dummy_vars.index = neighbors.index
+    dummy_vars = pd.concat([neighbors,
+                            dummy_vars],
+                            axis=1)
     return dummy_vars
