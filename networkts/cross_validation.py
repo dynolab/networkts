Fixed AR conf. intervals, Exp. Smoothing transform. Added its inverse transform. Fixed data leakage and inaccuracies while evaluated model. #13

networkts/cross_validation.py
@@ -102,29 +102,33 @@ class ValidationBasedOnRollingForecastingOrigin:
         self.n_splits = n_splits
         self.max_train_size = max_train_size
 
-    def evaluate(self, forecaster, y, X):
+    def evaluate(self, forecaster, data, dummy_vars, original_data, log):
         """Evaluate the performance of the given forecaster using
         the time series passed via `y` and splitting it into
         training/validation pieces as specified by the constructor.
 
         Parameters
         ----------
-        y : array-like, shape (n_timesteps x n_endo)
-            Time series which will be split into training/validation pieces.
-        X : array-like or function, shape (n_timesteps x n_exo), optional
-            (default=None). Exogeneous time series to adjust to. Can also
-            be understood as a control signal or some externally available
-            information.
+        data:           array-like, shape (n_timesteps x n_endo)
+                        Time series which will be split into training/validation pieces.
+        dummy_vars:     array-like or function, shape (n_timesteps x n_exo), optional
+                        (default=None). Exogeneous time series to adjust to. Can also
+                        be understood as a control signal or some externally available
+                        information.
+        original_data:  array-like, shape (n_timesteps x n_endo)
+                        Time series will be used to calculate score metrics.
+        log:            boolean
+                        Requirement of inverse logarithm transform.
 
         Returns
         -------
         A list of metric values for each split
         """
-        return [self.score(y_true, y_pred)
-                for _, y_pred, y_true in self.prediction_generator(
+        return [self.score(y_true, y_pred, original_data[test_ind], log)
+                for test_ind, y_pred, y_true in self.prediction_generator(
                                                 forecaster,
-                                                y,
-                                                X
+                                                data,
+                                                dummy_vars
                                                 )]
 
     def prediction_generator(self, forecaster, y, X):
@@ -231,17 +235,22 @@ class ValidationBasedOnRollingForecastingOrigin:
         grid.fit(X=X, y=y)
         return grid
 
-    def score(self, y_true, y_pred):
+    def score(self, y_true, y_pred, y_origin=None, log=True):
         score = []
-        if len(y_true.shape) > 1:
-            y_true = y_true[:, 0]
+        if log:
+            y_true = np.exp(y_true)
+            y_pred = np.exp(y_pred)
+        if y_origin is None:
+            y_origin = y_true
+        if len(y_origin.shape) > 1:
+            y_origin = y_origin[:, 0]
         if len(y_pred.shape) > 1:
             y_pred = y_pred[:, 0]
-        y_true = y_true.flatten()
+        y_origin = y_origin.flatten()
         y_pred = y_pred.flatten()
         for m in self.metric:
             try:
-                score.append(m(y_pred=y_pred, y_true=y_true))
+                score.append(m(y_pred=y_pred, y_true=y_origin))
             except:
                 score.append(None)
         return score

networkts/decompositions/standard.py
@@ -80,25 +80,32 @@ class ESTarget(object):
         super().__init__()
 
     def transform(self, y):
-        if len(y.shape) < 2:
-            s = ExponentialSmoothing(y).fit(
-                                        smoothing_level=self.smoothing_level,
-                                        optimized=False
-                                        )
-            res = s.fittedvalues
-        else:
-            res = np.empty(y.shape)
-            for j in range(y.shape[1]):
-                s = ExponentialSmoothing(y[:, j]).fit(
-                                        smoothing_level=self.smoothing_level,
-                                        optimized=False
-                                        )
-                res[:, j] = s.fittedvalues
+        alpha = self.smoothing_level
+        fl = False
+        if len(y.shape)==1:
+            y = y.reshape((y.shape[0], 1))
+            fl = True
+        res = np.zeros(y.shape)
+        res[0, :] = y[0, :]
+        for i in range(1, y.shape[0]):
+            res[i, :] = alpha*y[i, :] + (1-alpha)*res[i-1, :]
+        if fl:
+            return res.flatten()
         return res
 
     def inverse_transform(self, y):
-        # do nothing
-        return y
+        alpha = self.smoothing_level
+        fl = False
+        if len(y.shape)==1:
+            y = y.reshape((y.shape[0], 1))
+            fl = True
+        res = np.zeros(y.shape)
+        res[0, :] = y[0, :]
+        for i in range(1, y.shape[0]):
+            res[i, :] = y[i, :]/alpha - (1-alpha)/alpha*y[i-1, :]
+        if fl:
+            return res.flatten()
+        return res
 
 
 class MATarget(object):